7d0ae4ade3d9172dc69265b4e285a2a724e55d68
Karl George <karlgeorge87@gmail.com>
2025-10-14 11:24:25 +0200

    Update card styles

--- components/StocksCard.tsx
+++ components/StocksCard.tsx
@@ -1,10 +1,10 @@
-import { ArrowDown, ArrowUp, Star } from 'lucide-react';
+import { ArrowDownRight, ArrowUpRight, Star } from 'lucide-react';
 import Image from 'next/image';
 
 const StocksCard = ({ company, logo, symbol, price, changePercent }: Stock) => {
   return (
-    <div className='flex flex-col bg-gray-bg p-4 rounded-lg w-72'>
-      <div className='flex items-center justify-between'>
+    <div className='flex flex-col bg-[#1C1D21] p-4 rounded-lg w-72 lg:w-1/5'>
+      <div className='flex items-center'>
         <Image
           src={logo}
           alt={company}
@@ -15,24 +15,25 @@ const StocksCard = ({ company, logo, symbol, price, changePercent }: Stock) => {
         <div>
           <div className='flex flex-col ml-4'>
             <span className='font-semibold'>{symbol}</span>
-            <span className='text-sm'>{company}</span>
+            <span className='text-sm text-[#65676B]'>{company}</span>
           </div>
         </div>
-        <div>
+        <div className='ml-auto'>
           <Star className='w-5 h-5' />
         </div>
       </div>
-      <div className='flex mt-6 text-center justify-center gap-4 items-center'>
+      {/* Price Change */}
+      <div className='flex justify-center items-center mt-12 gap-2 flex-col'>
         <span className='text-xl'>${price}</span>
         <div className='flex items-center gap-1'>
           {changePercent.toString().charAt(0) === '-' ? (
             <div className='flex items-center gap-1'>
-              <ArrowDown className='w-4 h-4' fill='red' stroke='red' />
+              <ArrowDownRight className='w-4 h-4' fill='red' stroke='red' />
               <span className='text-red-500'>{changePercent.toFixed(2)}%</span>
             </div>
           ) : (
             <div className='flex items-center gap-1'>
-              <ArrowUp className='w-4 h-4' fill='green' stroke='green' />
+              <ArrowUpRight className='w-4 h-4' fill='green' stroke='green' />
               <span className='text-green-500'>
                 {changePercent.toFixed(2)}%
               </span>
