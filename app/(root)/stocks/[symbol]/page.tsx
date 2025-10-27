e9c4c21f0da9270c195dfdd7bb7e340687033055
Karl George <karlgeorge87@gmail.com>
2025-10-27 13:37:47 +0100

    Add stock details page

--- app/(root)/stocks/[symbol]/page.tsx
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -1,7 +1,15 @@
 import TradingViewWidget from '@/components/TradingViewWidget';
 import { getStocksDetails } from '@/lib/actions/finnhub.actions';
 import { getUserWatchlist } from '@/lib/actions/watchlist.actions';
-import { setDetailsWidget, setWidgetConfig } from '@/lib/utils';
+import {
+  setAdvancedWidget,
+  setDetailsWidget,
+  setFinancialsWidget,
+  setProfileWidget,
+  setTechAnalysisWidget,
+  setWidgetConfig,
+} from '@/lib/utils';
+import { set } from 'mongoose';
 import { notFound } from 'next/navigation';
 
 const StockDetails = async ({
@@ -20,25 +28,48 @@ const StockDetails = async ({
   if (!stock) notFound();
 
   return (
-    <div className='flex min-h-screen pt-10'>
-      <section className='grid grid-cols-1 lg:grid-cols-2 gap-8 w-full'>
+    <div className='flex min-h-screen py-10'>
+      <section className='grid grid-cols-1 lg:grid-cols-3  gap-8 w-full'>
         {/* Left */}
-        <div className='flex flex-col gap-5'>
+        <div className='flex flex-col gap-5 lg:col-span-2'>
           <TradingViewWidget
             scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-symbol-info.js'
             config={setDetailsWidget(symbol)}
-            height={260}
+            height={250}
           />
 
           <TradingViewWidget
             scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js'
             config={setWidgetConfig(symbol)}
-            height={850}
+            height={600}
+          />
+
+          <TradingViewWidget
+            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js'
+            config={setAdvancedWidget(symbol)}
+            height={600}
           />
         </div>
 
         {/* Right */}
-        <div className='flex flex-col gap-5'></div>
+        <div className='flex flex-col gap-5 lg:col-span-1'>
+          <TradingViewWidget
+            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-financials.js'
+            config={setFinancialsWidget(symbol)}
+            height={650}
+          />
+          <TradingViewWidget
+            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js'
+            config={setTechAnalysisWidget(symbol)}
+            height={400}
+          />
+
+          <TradingViewWidget
+            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-symbol-profile.js'
+            config={setProfileWidget(symbol)}
+            height={400}
+          />
+        </div>
       </section>
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -53,5 +53,71 @@ export const setDetailsWidget = (symbol: string) => {
     isTransparent: false,
     locale: 'en',
     width: '100%',
+    height: '100%',
+  };
+};
+
+export const setAdvancedWidget = (symbol: string) => {
+  return {
+    allow_symbol_change: true,
+    calendar: false,
+    details: false,
+    hide_side_toolbar: true,
+    hide_top_toolbar: false,
+    hide_legend: false,
+    hide_volume: false,
+    hotlist: false,
+    interval: 'D',
+    locale: 'en',
+    save_image: true,
+    style: '1',
+    symbol: `NASDAQ:${symbol}`,
+    theme: 'dark',
+    timezone: 'Etc/UTC',
+    backgroundColor: '#0F0F0F',
+    gridColor: 'rgba(242, 242, 242, 0.06)',
+    watchlist: [],
+    withdateranges: false,
+    compareSymbols: [],
+    studies: [],
+    autosize: true,
+  };
+};
+
+export const setFinancialsWidget = (symbol: string) => {
+  return {
+    symbol: `NASDAQ:${symbol}`,
+    colorTheme: 'dark',
+    displayMode: 'regular',
+    isTransparent: false,
+    locale: 'en',
+    width: '100%',
+    height: 650,
+  };
+};
+
+export const setTechAnalysisWidget = (symbol: string) => {
+  return {
+    colorTheme: 'dark',
+    displayMode: 'single',
+    isTransparent: false,
+    locale: 'en',
+    interval: '1m',
+    disableInterval: false,
+    width: '100%',
+    height: 400,
+    symbol: `NASDAQ:${symbol}`,
+    showIntervalTabs: true,
+  };
+};
+
+export const setProfileWidget = (symbol: string) => {
+  return {
+    symbol: `NASDAQ:${symbol}`,
+    colorTheme: 'dark',
+    isTransparent: false,
+    locale: 'en',
+    width: '100%',
+    height: 400,
   };
 };
